7f4bd31ee55f79caa90e9b4d4d38f558df29b191
jeondaen <jeondaen@gmail.com>
2022-05-18 21:06:14 +0900

    prework for remove cache

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://tichdaen.github.io/tichdaen.io',
+  base: 'https://tichdaen.github.io/tichdaen.io/',
   plugins: [react()]
 })
+
+// TODO: Main Branch Push => gh-pages deploy 실행
