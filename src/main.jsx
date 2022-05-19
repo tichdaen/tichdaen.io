ac1c7451ab440ee1d73fc050e262d8dda6e710b9
jeondaen <jeondaen@gmail.com>
2022-05-20 03:16:50 +0900

    update baseUrl

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/tichdaen.io'>
       <Header />
       <Routes>
         <Route path='/' element={<App />} />
