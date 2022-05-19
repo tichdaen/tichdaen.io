77ecd741b9cf0f031adb131049296274901806d7
jeondaen <jeondaen@gmail.com>
2022-05-20 03:12:25 +0900

    updates vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://tichdaen.github.io/',
+  base: 'https://tichdaen.github.io/tichdaen.io/',
   plugins: [react()]
 })
 
